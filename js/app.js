6a0f5e45d2c2b19f92bbc6fea21598e73ba62801
Mehedi390 <mh0816150@gmail.com>
2021-09-01 12:09:20 -0700

    result show

--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@ document.getElementById('button').addEventListener('click', function () {
     const inputFlid = document.getElementById('input');
     const inputValue = inputFlid.value;
     inputFlid.value = '';
-    getBooks(inputValue)
-
+    if (inputValue === '') {
+        alert('please write something');
+    }
+    else { getBooks(inputValue); }
     // console.log(inputValue);
-})
+});
 // fetch api -------------------------------
 const getBooks = (bookName) => {
     fetch(`http://openlibrary.org/search.json?q= ${bookName}`)
@@ -15,26 +17,35 @@ const getBooks = (bookName) => {
         .then(data => displayBook(data.docs))
 }
 const displayBook = books => {
+    const totalResult = document.getElementById('total-result');
+    const h1 = document.createElement('h1');
+    h1.innerText = `Total Result :  ${books.length}`;
+    totalResult.appendChild(h1)
     const container = document.getElementById('container');
-
-    // console.log(books);
-    books.forEach(book => {
-        console.log(book);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-        <div class="card rounded">
-                    <img src="https://covers.openlibrary.org/b/id/${book.cover_i}.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${book.title}</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
-                    </div>
-        </div>
-        
-        `;
-        container.appendChild(div);
-
-
-    })
+    console.log(books.length);
+    if (books.length === 0) {
+        alert('Your Result is Not Found')
+    }
+    else {
+        // console.log(books);
+        books.forEach(book => {
+            console.log(book);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card rounded">
+                        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}.jpg" class="card-img-top img" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Book Name : ${book.title}</h5>
+                            <p>Author Name : ${book.author_name}</p>
+                            <p> Frist Published : ${book.publish_date}</p>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.</p>
+                        </div>
+            </div>
+            `;
+            container.appendChild(div);
+        })
+    }
 }
+//-----------
